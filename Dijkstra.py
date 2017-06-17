bc2395d8d608698df1d635b4d95a867fc638990b
c0deb0t <daniel.liu02@gmail.com>
2017-06-17 15:35:05 -0700

    Add files via upload

--- Dijkstra.py
+++ Dijkstra.py
@@ -3,21 +3,25 @@ from heapq import heappush, heappop
 
 def dijkstra_shortest_path(graph, start, end):
     dist = [None] * len(graph)
-    pq = [(0, start)]
-    prev = [-1] * len(graph)
+    pq = [(0, start, None)]
+    prev = [None] * len(graph)
     while pq:
-        u_dist, u = heappop(pq)
+        u_dist, u, u_prev = heappop(pq)
         if dist[u] is None:
             dist[u] = u_dist
+            prev[u] = u_prev
             for v in range(len(graph[u])):
                 if graph[u][v] is not None and dist[v] is None:
-                    heappush(pq, (u_dist + graph[u][v], v))
-                    prev[v] = u
+                    heappush(pq, (u_dist + graph[u][v], v, u))
     path = []
     curr = end
-    while curr != -1:
+    while curr != None:
         path.insert(0, curr)
         curr = prev[curr]
     return dist[end], path
 
-print(dijkstra_shortest_path([[None, 2, 4], [None, None, 1], [None, None, None]], 0, 2))
+print(dijkstra_shortest_path([[None, 5, 3, 4, None],
+                              [5, None, 1, None, 2],
+                              [3, 1, None, None, None],
+                              [4, None, None, None, 4],
+                              [None, 2, None, 4, None]], 0, 4))
